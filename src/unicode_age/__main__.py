00c48e16e679d2f17a5cd59e72cf28c27bca3f22
Jeff Epler <jepler@unpythonic.net>
2026-09-22 13:04:51 -0500

    add a debug flag to the commandline program

--- src/unicode_age/__main__.py
+++ src/unicode_age/__main__.py
@@ -3,6 +3,7 @@ import string
 import unicodedata
 
 from . import version
+from .unicode_age_db import iter_spans
 
 def parse_codepoint(s):
     s = s.lower()
@@ -17,21 +18,37 @@ def parse_codepoint(s):
 def main():
     parser = argparse.ArgumentParser(
             description="Print the version of the Unicode standard where a code point was added")
-    parser.add_argument("codepoints", metavar="codepoints", nargs="+", type=str,
+    parser.add_argument("--debug", metavar="debug", action=argparse.BooleanOptionalAction, help="Show information for debugging the version table")
+    parser.add_argument("codepoints", metavar="codepoints", nargs="*", type=str,
                         help="Codepoints to act on")
     args = parser.parse_args()
+
+    if args.debug:
+        for i, pi in zip(range(10), iter_spans()):
+            print(i, pi)
+        return
+
+    if not args.codepoints:
+        parser.parse_args(["--help"])
+        return
+
     for cp in args.codepoints:
         i = parse_codepoint(cp)
         try:
             major, minor = version(i)
             vstr = f"{major}.{minor}"
         except ValueError:
-            vstr="Unk"
+            vstr="None"
         uplus = f"U+{i:x}"
         try:
             name = f"\"\\N{{{unicodedata.name(chr(i)).lower()}}}\""
         except ValueError:
-            name = f"\"\\U{i:06x}\""
+            if i < 256:
+                name = f"\"\\x{i:02x}\""
+            elif i < 65536:
+                name = f"\"\\u{i:04x}\""
+            else:
+                name = f"\"\\U{i:06x}\""
         print(f"{uplus:<8} {name:<60} {vstr:>5}")
 
 if __name__ == '__main__':
